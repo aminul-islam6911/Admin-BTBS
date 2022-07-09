dd18cdc137b56ffb2a64a6f66901db1115a312e0
aminul-islam6911 <aikayoum420@gmail.com>
2022-07-10 00:13:57 +0600

    First attempt

--- app/src/main/java/com/example/adminbtbs/Interface/IFirebaseLoadDone.java
+++ app/src/main/java/com/example/adminbtbs/Interface/IFirebaseLoadDone.java
@@ -1,11 +1,11 @@
 package com.example.adminbtbs.Interface;
+
 import com.example.adminbtbs.Model.IDs;
 
 import java.util.List;
 
 public interface IFirebaseLoadDone {
     void onFirebaseLoadSuccess(List<IDs> LocationList);
+
     void onFirebaseLoadFailed(String Message);
 }
-
-
